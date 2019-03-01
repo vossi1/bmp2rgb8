239b08d7336a626e4fd6f0e2958ce477bdea65a9
Christian Forstreuter <vossi@ceffy.de>
2019-03-01 08:34:31 +0100

    1.2

--- bmp2rgb8.c
+++ bmp2rgb8.c
@@ -1,10 +1,10 @@
 // bmp2rgb8.c
-// converts 24bit bmp files to 8bit rgb for V99x8 mode 7
-// written by Vossi on 01/2019 in Hamburg/Germany
-// version 1.1 copyright (c) 2019 Vossi. All rights reserved.
+// converts 24bit windows bmp files to 8bit rgb for V99x8 mode 7
+// designed by Vossi on 01/2019 in Hamburg/Germany
+// version 1.2 copyright (c) 2019 Vossi. All rights reserved.
 
 // option -p outputs a bmp file with color reduced to 8bit rgb
-// needs top-to-bottom bmp with negative heigh!
+// turns upside down bmp
 
 // rgb format: 1. byte x resolution (1-255. 0=256)
 //             2. byte y resolution (1-212)
@@ -21,11 +21,11 @@ static int preview;
 int main(int argc,char *argv[])
 {
   FILE *f;
-  int i, n;
+  int i, n, x, y;
   byte buf[256];
   byte rg[8] = {0,0x24,0x49,0x6d,0x92,0xb6,0xdb,0xff};
   word headersize;
-  word xres, yres;
+  word xres, yres, xrgb;
 
   preview=0;
 
@@ -39,8 +39,8 @@ int main(int argc,char *argv[])
   
   if(n==argc)  
   {
-    fprintf(stderr,"bmp2rgb8 v.1.1 by Vossi 01/2019\n");
-    fprintf(stderr,"converts 24bit bmp files to 8bit rgb for V99x8 mode 7\n");
+    fprintf(stderr,"bmp2rgb8 v.1.2 by Vossi 01/2019\n");
+    fprintf(stderr,"converts 24bit windows bmp files to 8bit rgb for V99x8 mode 7\n");
     fprintf(stderr,"usage: %s [-p] file\n",argv[0]);
     fprintf(stderr,"  -p - preview -> bmp reduced to 8bit rgb colors;)\n");
     return(1);
@@ -61,17 +61,24 @@ int main(int argc,char *argv[])
         {
           if(preview) for(i=0;i<headersize;i++) printf("%c",buf[i]);
           xres=buf[0] + 256 * buf[1];
-          yres=0 - (buf[4] + 256 * buf[5]);
+          yres=buf[4] + 256 * buf[5];
           if(xres > 0 && yres > 0 && xres <= 256 && yres <= 212)
           {
-            if(xres == 256) xres = 0;				// 0 -> 256
-            if(!preview) printf("%c%c", (char)xres, (char)yres);
-            while(fread(buf,3,1,f))
-            {
-              if(preview)
+            if(preview)
+              while(fread(buf,3,1,f))
                 printf("%c%c%c",((buf[0]&0xc0)>>6)*0x55, rg[(buf[1]&0xe0)>>5], rg[(buf[2]&0xe0)>>5]);
-              else
-                printf("%c",(buf[1]&0xe0)+((buf[2]>>3)&0x1c)+(buf[0]>>6));
+            else
+            {
+              byte databuf[xres*yres*3];
+              if(fread(databuf,xres*yres*3,1,f))
+              {
+                xrgb = xres;
+                if(xrgb == 256) xrgb = 0;       // 0 -> 256
+                printf("%c%c", (char)xrgb, (char)yres);
+                for(y=yres-1;y>=0;y--)
+                  for(x=0;x<xres;x++)
+                    printf("%c",(databuf[y*xres*3+x*3+1]&0xe0)+((databuf[y*xres*3+x*3+2]>>3)&0x1c)+(databuf[y*xres*3+x*3]>>6));
+              }
             }
           }
           else
@@ -80,7 +87,7 @@ int main(int argc,char *argv[])
             	printf("BMP image size < 1!\n");  
             else 
               if(yres > 0x7fff)
-            		printf("BMP image is upside down!\n");  
+            		printf("BMP image is bottom to top!\n");  
             	else
             		printf("BMP image is too large (max x=256, y=212!\n");  
           }
